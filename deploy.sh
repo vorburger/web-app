fc0c6a33749ab7275b4a075c6311b7146bc157f3
Michael Vorburger <mike@vorburger.ch>
2020-05-06 03:04:10 +0200

    fix deploy.sh (see https://github.com/openMF/web-app/issues/737)

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# TODO unclear if really required if using "npm run build:prod" instead of "ng build -prod"
 npm install @angular/cli
 
-npm run build:prod
+./node_modules/.bin/ng build -prod
 
 firebase deploy --only hosting:openmf-web-app
